8b9ad04e95a20895b5ff79c0cac1ab87b91d840e
Егор <nnez252@gmail.com>
2023-02-10 08:16:47 +0200

    fdsf

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,59 @@
-//interface Predicate<T> {
-//    boolean predict(T t);
-//}
-//
-//@FunctionalInterface
-//interface Consumer<T> {
-//    void consum(T t);
-//}
-//interface Function<D,L> {
-//    D apply(L l);
-//
-//}
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        double x = 3125.312321d;
-//        long y = 3125;
-//        String name = "Elena";
-//        int number = -23;
-//        Predicate pred = new Predicate(){
-//            public boolean predict(Object o) {
-//                if (number > 0) {
-//                    System.out.println("true");
-//                } else {
-//                    System.out.println("false");
-//                }
-//                return false;
-//            }
-//        };
-//        pred.predict(number);
+import java.util.Objects;
 
-//        Predicate pred = (n) -> {
-//            if (number > 0) {
-//                System.out.println("true");
-//            } else {
-//                System.out.println("false");
-//            }
-//            return false;
-//        };
-//pred.predict(number);
+import static java.lang.Double.valueOf;
 
 
-//Consumer consum = new Consumer() {
-//    public void consum(Object o) {
-//        if (name != "") {
-//            System.out.println("Привет " + name);
-//        }
-//    }
-//};
-//consum.consum(name);
+interface Predicate<T> {
+    boolean test(T t);
+}
+
+interface Consumer<T> {
+    void accept(T t);
+}
+interface Function<T, R> {
+    R apply(T t);
+}
+@FunctionalInterface
+interface Supplier<T> {
+    T get();
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Predicate<Integer> number = x -> x > 0;
+        System.out.println(number.test(10));
 
 
-//        Consumer consum = (n) -> {
-//            if (name != "") {
-//                System.out.println("Привет "+ name);
-//            }
-//        };
-//        consum.consum(name);
+        Consumer<String> name = x -> System.out.println("Hello " + x);
+        name.accept("Elena");
 
-//        Function function = new Function() {
-//
-//    }
-//}
+
+        Function<Long, Double> numbers = x -> Double.valueOf(Long.valueOf(x));
+        System.out.println(numbers.apply((long) 23323232.32d));
+
+
+        Supplier<Integer> randomnumber = () -> (int) (Math.random() * 100);
+        System.out.println(randomnumber.get());
+
+
+
+        Predicate<Object> condition = Objects::isNull;
+        Function<Object, Integer> ifTrue = obj -> 0;
+        Function<CharSequence, Integer> ifFalse = CharSequence::length;
+        Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+
+        System.out.println(safeStringLength);
+
+
+
+
+
+    }
+
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+        return t -> condition.test(t) ? ifTrue.apply(t) : ifFalse.apply(t);
+    }
+}
